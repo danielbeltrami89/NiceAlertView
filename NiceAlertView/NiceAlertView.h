3d77cc2cd587d92fc82700a484dd20992af1f05b
Daniel Beltrami <moraesdan89@gmail.com>
2021-11-21 17:14:30 -0300

    Initial Commit

---
+++ NiceAlertView/NiceAlertView.h
@@ -0,0 +1,18 @@
+//
+//  NiceAlertView.h
+//  NiceAlertView
+//
+//  Created by Daniel Beltrami on 21/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NiceAlertView.
+FOUNDATION_EXPORT double NiceAlertViewVersionNumber;
+
+//! Project version string for NiceAlertView.
+FOUNDATION_EXPORT const unsigned char NiceAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NiceAlertView/PublicHeader.h>
+
+
